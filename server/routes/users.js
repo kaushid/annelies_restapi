613c9ebc2f92e1125a5e271a3e8be2838fb8d5df
kaushid <dey.kaushikd@gmail.com>
2018-01-06 03:13:47 +0530

    Fix : Add user only if there is no other user with same email

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,44 +3,60 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const serverConfig = require('../server.config');
-const Users = require('../models/user');
+const User = require('../models/user');
 //userd by bcrypt to add 10 random letters to password and then hash it 
 const saltRounds = 10;
 
 router.post('/signup', function (req, res, next) {
-
-    /**
-     * Hashing the password before saving to DB (Hashing One way process)
-     */
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        // Store hash in your password DB.
-        if (err) {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(function (user) {
+            //user is an empty array
+            if (user.length) {               
+                //409 - conflict - resource already exists
+                return res.status(409).json({
+                    message: 'Email already used up'
+                });
+            } else {
+                /**
+                 * Hashing the password before saving to DB (Hashing One way process)
+                 */
+                bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                    // Store hash in your password DB.
+                    if (err) {
+                        return res.status(500).json({
+                            message: 'Unable to save the user',
+                            error: err
+                        })
+                    } else {
+                        const user = new Users({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            name: req.body.name,
+                            password: hash
+                        });
+                        user.save()
+                            .then(function (result) {
+                                res.status(201).json({
+                                    message: 'User Created'
+                                });
+                            })
+                            .catch(function (err) {
+                                return res.status(500).json({
+                                    message: 'Unable to save the user',
+                                    error: err
+                                })
+                            });
+                    }
+                });
+            }
+        })
+        .catch(function (err) {
             return res.status(500).json({
                 message: 'Unable to save the user',
                 error: err
             })
-        } else {
-            const user = new Users({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                name : req.body.name,
-                password: hash
-            });
-            user.save()
-                .then(function (result) {
-                    res.status(201).json({
-                        message: 'User Created'
-                    });
-                })
-                .catch(function (err) {
-                    return res.status(500).json({
-                        message: 'Unable to save the user',
-                        error: err
-                    })
-                });
-        }
-    });
-
+        });
 });
 
 module.exports = router;
