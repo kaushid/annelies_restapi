6d8b4021b5abb1d5f13357c9c703a234816a09d2
kaushid <dey.kaushikd@gmail.com>
2018-01-06 04:04:54 +0530

    Generating JWT and returning it on success login

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const serverConfig = require('../server.config');
 const User = require('../models/user');
 //userd by bcrypt to add 10 random letters to password and then hash it 
@@ -59,19 +60,63 @@ router.post('/signup', function (req, res, next) {
         });
 });
 
-router.delete('/:userId', function (req, res, next) {
-    User.remove({_id : req.parama.userId})
-    .exec()     // return a promise object
-    .then(function(result){
-        res.status(200).json({
-            message : 'User Deleted'
-        });
-    })
-    .catch(function (err) {
-        return res.status(500).json({
-            message: 'Unable to delete the user',
-            error: err
+
+router.post('/signin', function (req, res, next) {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(function (user) {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: 'Authentication Failed'
+                });
+            }
+            //Checkin the hashed password
+            // Load hash from your password DB.            
+            bcrypt.compare(req.body.password, user[0].password, function (err, result) {
+                if (result) {
+
+                    //create jwt token 
+                    var jwtToken = jwt.sign(
+                        {
+                            email: user[0].email,
+                            userName: user[0].name,
+                            userId: user[0]._id
+                        },
+                        serverConfig.jwtSecretKey,
+                        { expiresIn: "1h" }
+                    );
+
+                    return res.status(201).json({
+                        message: 'Authentication Successful',
+                        jwtToken : jwtToken
+                    });
+                }
+                return res.status(401).json({
+                    message: 'Authentication Failed'
+                });
+            });
         })
-    });
+        .catch(function (err) {
+            return res.status(500).json({
+                message: 'Authentication Failed',
+                error: err
+            })
+        });
+});
+
+router.delete('/:userId', function (req, res, next) {
+    User.remove({ _id: req.parama.userId })
+        .exec()     // return a promise object
+        .then(function (result) {
+            res.status(200).json({
+                message: 'User Deleted'
+            });
+        })
+        .catch(function (err) {
+            return res.status(500).json({
+                message: 'Unable to delete the user',
+                error: err
+            });
+        });
 });
 module.exports = router;
--- server/server.config.js
+++ server/server.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     'mongoLabUrl' : 'mongodb://admin:admin@ds239587.mlab.com:39587/ng2_annelies',
     'mongoAtlasUrl' : 'mongodb://admin:admin@annelies-shard-00-00-n6p0z.mongodb.net:27017,annelies-shard-00-01-n6p0z.mongodb.net:27017,annelies-shard-00-02-n6p0z.mongodb.net:27017/test?ssl=true&replicaSet=annelies-shard-0&authSource=admin',
-    'apiHostedBaseUrl' : 'http://localhost:3000/'
+    'apiHostedBaseUrl' : 'http://localhost:3000/',
+    'jwtSecretKey' : 'mySecretKey'
 }
